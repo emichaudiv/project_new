10436ff88c1a78ca181bd318243cba37b5c91804
edward-michaud <122934951+emichaudiv@users.noreply.github.com>
2023-11-02 22:40:11 -0500

    Add files via upload

--- main.gd
+++ main.gd
@@ -1,5 +1,5 @@
 extends Node2D
-#hello
+#helloworld
 
 func _on_quit_pressed():
 	get_tree().quit()
@@ -7,3 +7,6 @@ func _on_quit_pressed():
 
 func _on_play_pressed():
 	get_tree().change_scene_to_file("res://lvl_1.tscn")
+
+func _on_settings_pressed():
+	get_tree().change_scene_to_file("res://set.tscn")
